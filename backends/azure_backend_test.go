a4147f1836c9a075d177a9d8c038598220b098c9
Johnathan Falk <johnathan.falk@gmail.com>
2022-11-13 10:32:42 -0500

    chore: Replace github repo but keep license.
    Signed-off-by: Johnathan Falk <johnathan.falk@gmail.com>

--- backends/azure_backend_test.go
+++ backends/azure_backend_test.go
@@ -1,4 +1,4 @@
-// Copyright © 2016 Prateek Malhotra (someone1@gmail.com)
+// Copyright © 2016 Prateek Malhotra (someone1)
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
